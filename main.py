f98859620ffc574661cd0220ff01ebeb544d5a44
Z3R0 <timothyzhang51@gmail.com>
2022-02-27 10:22:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telegram, logging
 from telegram import Update, Bot
-from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler
+from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 
 ## token
 token = "5224120947:AAH8J6SzRjiafxnAQJMKwRz-zDs14Qs6Dq4"
@@ -19,7 +19,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 ##func
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+def unknown(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 start_handler = CommandHandler('start', start)
+unknown_handler = MessageHandler(Filters.command, unknown)
 dp.add_handler(start_handler)
+dp.add_handler(echo_handler)
+dp.add_handler(unknown_handler)
 updater.start_polling()
